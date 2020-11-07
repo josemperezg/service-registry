e9c4eb6eb38c399f2b0f494f921984fa599d752b
Jose Perez <josemperezg@gmail.com>
2020-11-07 08:08:49 -0600

    Unregistering services

--- server/lib/ServiceRegistry.js
+++ server/lib/ServiceRegistry.js
@@ -18,9 +18,14 @@ class ServiceRegistry {
       this.log.debug(`Added services ${name}, version ${version} at ${ip}:${port}`);
       return key;
     }
-    //Case when the service is already in the register
     this.services[key].timestamp = Math.floor(new Date() / 1000);
     this.log.debug(`Updated services ${name}, version ${version} at ${ip}:${port}`);
+    return key;
+  }
+
+  unregister(name, version, ip, port) {
+    const key = name + version + ip + port;
+    delete this.services[key];
     return key;
   }
 }
